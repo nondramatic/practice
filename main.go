5ce4886f666f90b086d958167703c7869859f7d1
nondramatic <mostsun1987@163.com>
2024-08-19 19:24:03 +0800

    seed to rand.Int63

--- main.go
+++ main.go
@@ -2,32 +2,29 @@ package main
 
 import (
 	"math/rand"
-	"strings"
 )
 
 func main() {
 
-	println(wordGen(100, 10))
+	m := wordGen(100, 10)
+	println(m[0], m[1])
 
 }
 
-func wordGen(nDistinct, wordLen int) func() string {
+func wordGen(nDistinct, wordLen int) []string {
 	vocab := make([]string, nDistinct)
 	for i := range nDistinct {
 		word := randomString(wordLen)
 		vocab[i] = word
 	}
-	return func() string {
-		word := vocab[rand.Intn(nDistinct)]
-		return strings.Clone(word)
-	}
+	return vocab
 }
 
 func randomString(n int) string {
 	// 脑子进煎鱼了
 	const letters = "eddycjyabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	ret := make([]byte, n)
-	r := rand.New(rand.NewSource(1))
+	r := rand.New(rand.NewSource(rand.Int63()))
 	for i := 0; i < n; {
 		b := make([]byte, 1)
 		if _, err := r.Read(b); err != nil {
