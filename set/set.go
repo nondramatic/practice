cf2c15c245bae894011fc4a79e6c4e33c7f08cc9
nondramatic <mostsun1987@163.com>
2024-08-20 18:23:19 +0800

    不可导出

--- set/set.go
+++ set/set.go
@@ -3,103 +3,103 @@ package set
 import "sync"
 
 type Set[T comparable] struct {
-	sync.RWMutex
-	Size int64
-	Data map[T]struct{}
+	mu   sync.RWMutex
+	size int64
+	data map[T]struct{}
 }
 
 // NewSet initializes and returns a new Set
 func NewSet[T comparable]() *Set[T] {
-	return &Set[T]{Data: make(map[T]struct{})}
+	return &Set[T]{data: make(map[T]struct{})}
 }
 
 // Add inserts a new element into the Set
 func (s *Set[T]) Add(element T) (bool, int64) {
-	s.Lock()
-	defer s.Unlock()
-	if _, exists := s.Data[element]; !exists {
-		s.Data[element] = struct{}{}
-		s.Size++
-		return true, s.Size
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, exists := s.data[element]; !exists {
+		s.data[element] = struct{}{}
+		s.size++
+		return true, s.size
 	}
 
-	return false, s.Size
+	return false, s.size
 }
 
 // AddSome inserts some elements into the Set
 func (s *Set[T]) AddSome(elements ...T) {
-	s.Lock()
-	defer s.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	for _, element := range elements {
-		if _, exists := s.Data[element]; !exists {
-			s.Data[element] = struct{}{}
-			s.Size++
+		if _, exists := s.data[element]; !exists {
+			s.data[element] = struct{}{}
+			s.data++
 		}
 	}
 }
 
 // RemoveSome deletes some elements from the Set
 func (s *Set[T]) RemoveSome(elements ...T) {
-	s.Lock()
-	defer s.Unlock()
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	for _, element := range elements {
-		if _, exists := s.Data[element]; exists {
-			delete(s.Data, element)
-			s.Size--
+		if _, exists := s.data[element]; exists {
+			delete(s.data, element)
+			s.size--
 		}
 	}
 }
 
 // Remove deletes an element from the Set
 func (s *Set[T]) Remove(element T) (bool, int64) {
-	s.Lock()
-	defer s.Unlock()
-	if _, exists := s.Data[element]; exists {
-		delete(s.Data, element)
-		s.Size--
-		return true, s.Size
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, exists := s.data[element]; exists {
+		delete(s.data, element)
+		s.size--
+		return true, s.size
 	}
 
-	return false, s.Size
+	return false, s.size
 }
 
 // Contains checks if an element exists in the Set
 func (s *Set[T]) Contains(element T) bool {
-	s.RLock()
-	defer s.RUnlock()
-	_, exists := s.Data[element]
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	_, exists := s.data[element]
 	return exists
 }
 
 // Len returns the number of elements in the Set
 func (s *Set[T]) Len() int64 {
-	s.RLock()
-	defer s.RUnlock()
-	return s.Size
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.size
 }
 
 // IsEmpty checks if the Set is empty
 func (s *Set[T]) IsEmpty() bool {
-	s.RLock()
-	defer s.RUnlock()
-	return s.Size == 0
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.size == 0
 }
 
 // Clear removes all elements from the Set
 func (s *Set[T]) Clear() {
-	s.Lock()
-	defer s.Unlock()
-	s.Data = make(map[T]struct{})
-	s.Size = 0
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.data = make(map[T]struct{})
+	s.size = 0
 }
 
 // Union returns a new Set with elements from both Sets
 func (s *Set[T]) Union(o *Set[T]) *Set[T] {
 	unionSet := NewSet[T]()
-	for k := range s.Data {
+	for k := range s.data {
 		unionSet.Add(k)
 	}
-	for k := range o.Data {
+	for k := range o.data {
 		unionSet.Add(k)
 	}
 	return unionSet
@@ -108,7 +108,7 @@ func (s *Set[T]) Union(o *Set[T]) *Set[T] {
 // Intersection returns a new Set with elements common to both Sets
 func (s *Set[T]) Intersection(o *Set[T]) *Set[T] {
 	intersectionSet := NewSet[T]()
-	for k := range s.Data {
+	for k := range s.data {
 		if o.Contains(k) {
 			intersectionSet.Add(k)
 		}
@@ -119,7 +119,7 @@ func (s *Set[T]) Intersection(o *Set[T]) *Set[T] {
 // Difference returns a new Set with elements in the current Set but not in the other
 func (s *Set[T]) Difference(o *Set[T]) *Set[T] {
 	differenceSet := NewSet[T]()
-	for k := range s.Data {
+	for k := range s.data {
 		if !o.Contains(k) {
 			differenceSet.Add(k)
 		}
@@ -130,12 +130,12 @@ func (s *Set[T]) Difference(o *Set[T]) *Set[T] {
 // SymmetricDifference returns a new Set with elements in either Set, but not both
 func (s *Set[T]) SymmetricDifference(o *Set[T]) *Set[T] {
 	symmetricDifferenceSet := NewSet[T]()
-	for k := range s.Data {
+	for k := range s.data {
 		if !o.Contains(k) {
 			symmetricDifferenceSet.Add(k)
 		}
 	}
-	for k := range o.Data {
+	for k := range o.data {
 		if !s.Contains(k) {
 			symmetricDifferenceSet.Add(k)
 		}
@@ -146,11 +146,11 @@ func (s *Set[T]) SymmetricDifference(o *Set[T]) *Set[T] {
 // Subset checks if the Set is a subset of another Set
 func (s *Set[T]) Subset(o *Set[T]) bool {
 
-	if s.Size > o.Size {
+	if s.size > o.size {
 		return false
 	}
 
-	for k := range s.Data {
+	for k := range s.data {
 		if !o.Contains(k) {
 			return false
 		}
